21b6a64893421c6cd37d96ed73ccf5ec718969cd
Naoya Matsumoto <k016c1346@it-neec.jp>
2019-06-25 15:58:33 +0900

    ホーム画面にそば子と時計を追加

---
+++ Assets/Scenes/Home/Scripts/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public Text date;
+    public Text time;
+    public Text week;
+    private float span = 0.5f;
+
+    void Start ()
+    {
+        ClockUpdate();
+        InvokeRepeating("ClockUpdate", span, span);
+    }
+
+    void ClockUpdate ()
+    {
+        DateTime dateTime = DateTime.Now;
+        date.text = dateTime.ToString("MM / dd");
+        if (dateTime.Millisecond < 500) {
+            time.text = dateTime.ToString("HH : mm");
+        } else {
+            time.text = dateTime.ToString("HH   mm");
+        }
+        var culture = System.Globalization.CultureInfo.GetCultureInfo("en-US");
+        week.text = dateTime.ToString("ddd", culture);
+    }
+}
