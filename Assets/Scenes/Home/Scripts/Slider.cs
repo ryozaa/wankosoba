abfcf82f4e3ec4d32cf83782c66d0bfebee0dea9
Naoya Matsumoto <k016c1346@it-neec.jp>
2019-06-26 15:56:04 +0900

    ホーム画面の更新

---
+++ Assets/Scenes/Home/Scripts/Slider.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slider : MonoBehaviour
+{
+    public SliderManager sliderManager;
+    private Animator animator;
+    private bool isSelected = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+
+        // クリックされたときにOnClickedを呼び出す
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener(OnClicked);
+
+        trigger.triggers.Add(entry);
+    }
+
+    private void OnClicked(BaseEventData eventData)
+    {
+        if (isSelected) return;
+        sliderManager.UnSelectAll();
+        Select();
+    }
+
+    public void Select()
+    {
+        if (isSelected) return;
+        isSelected = true;
+        animator.Play("SlideIn");
+    }
+
+    public void UnSelect()
+    {
+        if (!isSelected) return;
+        isSelected = false;
+        animator.Play("SlideOut");
+    }
+}
---
+++ Assets/Scenes/Home/Scripts/SliderManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SliderManager : MonoBehaviour
+{
+    public List<Slider> sliders;
+
+    void Start()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+
+        // クリックされたときにOnClickedを呼び出す
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener(OnClicked);
+
+        trigger.triggers.Add(entry);
+    }
+
+    private void OnClicked(BaseEventData eventData)
+    {
+        UnSelectAll();
+    }
+
+    public void UnSelectAll()
+    {
+        foreach (Slider slider in sliders) {
+            slider.UnSelect();
+        }
+    }
+}
