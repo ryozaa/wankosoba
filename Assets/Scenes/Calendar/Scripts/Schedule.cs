bc5bc4abce9effab18b8daf4746cd2551cb7d1fd
Naoya Matsumoto <k016c1346@it-neec.jp>
2019-07-04 15:17:18 +0900

    スケジュール一覧画面を更新

---
+++ Assets/Scenes/Calendar/Scripts/Schedule.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Schedule : MonoBehaviour
+{
+    public GameObject SchedulePanel;
+    public GameObject AddPanel;
+    private List<SchedulePanel> scheduleList = new List<SchedulePanel>();
+
+    public void Start()
+    {
+    }
+
+    public void Add()
+    {
+        var clonePanel = (GameObject)Instantiate(SchedulePanel, new Vector2(), Quaternion.identity);
+        clonePanel.transform.SetParent(transform);
+
+        AddPanel.transform.SetAsLastSibling();
+    }
+}
---
+++ Assets/Scenes/Calendar/Scripts/ScheduleAddPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ScheduleAddPanel : MonoBehaviour
+{
+    public Schedule Schedule;
+
+    void Start()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+
+        // クリックされたときにOnClickを呼び出す
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener(OnClick);
+
+        trigger.triggers.Add(entry);
+    }
+
+    private void OnClick(BaseEventData eventData)
+    {
+        Schedule.Add();
+    }
+}
