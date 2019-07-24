a70e6dc5e46f20357c7359406a64d30223b8a5ab
Naoya Matsumoto <k016c1346@it-neec.jp>
2019-07-24 10:58:11 +0900

    CostumeTableとTodoTableを追加

---
+++ Assets/SQLite/CostumeTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CostumeTable
+{
+    private static string table = "costume"; 
+
+    public static DataTable FindById(int id)
+    {
+        return WankoDB.FindBy(table, "costume_id", $"'{id}'");
+    }
+
+    public static DataTable FindByType(int type)
+    {
+        return WankoDB.FindBy(table, "type", type.ToString());
+    }
+}
