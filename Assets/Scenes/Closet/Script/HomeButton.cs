c99fc44897bf9f05052c60561c40ad29e4ad3fb4
Naoya Matsumoto <k016c1346@it-neec.jp>
2019-12-02 10:39:39 +0900

    着せ替え画面の更新
    ホーム画面に戻るボタンのスクリプトを追加

---
+++ Assets/Scenes/Closet/Script/HomeButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HomeButton : MonoBehaviour
+{
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(() => {
+            SceneManager.LoadScene("Home");
+        });
+    }
+}
